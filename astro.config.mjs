63868cbf32f4ad112cadbbd2d8984f34025f89cd
Wojtek1441 <motyl.motyl@interia.pl>
2025-02-22 17:30:40 +0000

    content changes nav

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,7 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from '@astrojs/tailwind';
-
 import icon from 'astro-icon';
+import minify from 'astro-min';
 
 // https://astro.build/config
 export default defineConfig({
@@ -13,4 +12,5 @@ export default defineConfig({
         icon(),
     ],
     prefetch: true,
+    site: 'https://www.graf-it.co.uk',
 });
--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,40 +1,48 @@
 export const paths = {
     home: '/',
     about: '/o-nas',
-    opinie: 'opinie-klientow',
-    realizacje: 'realizacje',
-    cennik: 'cennik',
-    blog: 'blog',
-    kontakt: 'kontakt',
+    oferta: '/oferta',
+    opinie: '/opinie-klientow',
+    realizacje: '/realizacje',
+    cennik: '/cennik',
+    blog: '/blog',
+    kontakt: '/kontakt',
 };
 
 export const menu = [
     {
         label: 'O nas',
+        title: 'O nas',
         url: paths.about,
     },
     {
         label: 'Oferta',
-        url: paths.about,
+        title: 'Oferta',
+        url: paths.oferta,
     },
     {
         label: 'Realizacje',
+        title: 'Realizacje',
         url: paths.realizacje,
     },
     {
         label: 'Opinie',
+        title: 'Opinie',
         url: paths.opinie,
     },
     {
         label: 'Cennik',
+        title: 'Cennik',
         url: paths.cennik,
     },
     {
         label: 'Blog',
+        title: 'Blog',
         url: paths.blog,
     },
     {
         label: 'Kontakt',
+        title: 'Kontakt',
         url: paths.kontakt,
     },
 ];
